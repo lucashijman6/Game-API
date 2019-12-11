618e84db0da9a4cb8bb25b5e661c7a820230a6b8
Lucas <096128@hr.nl>
2019-12-11 19:48:18 +0100

    package & server
    dependency update en een andere poort

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -10,8 +11,9 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.error('Connected to database!'))
 
 app.use(express.json())
+app.use(cors())
 
 const gamesRouter = require('./routes/games')
 app.use('/games', gamesRouter)
 
-app.listen(8000, () => console.log('Server started!'))
+app.listen(8080, () => console.log('Server started!'))
