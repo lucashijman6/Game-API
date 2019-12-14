fc3d20c11cad6b52c9464022b67af8087f60ba43
Lucas <096128@hr.nl>
2019-12-14 12:18:28 +0100

    eigen package en url encoding
    Mijn dependencies, die samen mijn eigen package vormen, heb ik van naam veranderd. Tevens een url encoding functie toegevoegd.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -12,6 +13,8 @@ db.once('open', () => console.error('Connected to database!'))
 
 app.use(express.json())
 app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const gamesRouter = require('./routes/games')
 app.use('/games', gamesRouter)
